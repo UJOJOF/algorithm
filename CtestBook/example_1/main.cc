41192138c0833db8e5814141a5099d938920852d
donghwi <bqbq423@gmail.com>
2025-05-07 01:17:20 +0900

    0506_1 example 1

---
+++ CtestBook/example_1/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+template <typename T>
+class dynamic_array
+{
+    T* data;
+    size_t n;
+
+public:
+    dynamic_array(int n) {
+        this->n = n;
+        data = new T[n];
+    }
+};
